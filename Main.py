08d3635ab34372a298e2265469aee1f39cbf380b
Abhishek Tiwari <68350077+abhiti095@users.noreply.github.com>
2021-03-02 13:50:08 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,78 @@
+import seat_book
+
+Movie = seat_book.Booking()
+
+while True:
+    print("\n\nWelcome to BookMyMovie.com\nBookMyMovies brings booking tickets for movies to your fingertips\n")
+    print("\nMain-Menu:")
+    print("1. Show Seating Arrangement")
+    print("2. Book a seat")
+    print("3. Show Booking Details")
+    print("4. Statistics")
+    print("0. Exit")
+    n = input("Please select of your desired option:(for eg. 1, 2 or 3)\n")
+    if n == "1":
+        print("\n")
+        print("Cinema:\n")
+        print("__"*5+"\033[4mScreen This Way!\033[m"+"__"*5+"\n")
+        print('\n'.join('\t'.join(i) for i in Movie.arranged_seats))
+        print("\nPlease Note:")
+        print('i. '+"\033[1;32m"+"S"+"\033[m" +" = Unreserved Seats(Availble for Booking)")
+        print('ii. '+"\033[1;31m"+"B"+"\033[m" +" = reserved Seats(Not Availble for Booking)")
+        while True:
+            book_seat = input("Would you like to book a seat? y/n\n")
+            if  book_seat =="y" or book_seat == 'Y':
+                Movie.info_taking()
+                while True:
+                    q = input('would you like to book another seat? y/n\n')
+                    if q =="y" or q == 'Y':
+                        Movie.info_taking()
+                    elif q =='n' or q == 'N':
+                        print('Thank you for booking ticket with us!')
+                        break
+                    else:
+                        print("Please select a valid option")
+                break
+            elif book_seat =="N" or book_seat =="n":
+                print("\nDiverting you to Main Menu...\nThank you!")
+                break
+            
+            else:
+                print("Please select a valid option")
+    elif n=="2":
+        print("\n")
+        Movie.info_taking()
+        while True:
+            q = input('would you like to book another seat? y/n\n')
+            if q =="y" or q == 'Y':
+                Movie.info_taking()
+            elif q =='n' or q == 'N':
+                print('Diverting you to Main Menu...\nThank you for booking your ticket with BookMyMovie.com!')
+                break
+            else:
+                print("Please select a valid option")
+    elif n == "3":
+        print("\n")
+        if len(Movie.booking_info) == 1 :
+            print("No Bookings till now!!\n")
+            print('\n'.join('\t'.join(i) for i in Movie.booking_info))
+        else:
+            length_list = [len(element) for row in Movie.booking_info for element in row]
+            column_width = max(length_list)
+            for row in Movie.booking_info:
+                row = " ".join(element.ljust(column_width+1)for element in row)
+                print(row)
+        print('\n')
+    elif n =="4":
+        print("\nStatistics:\n")
+        print("Number of Ticket Purchased: ", len(Movie.seats_list))
+        print("Net Occupancy Rate(%):","{:.2f}".format(Movie.stats()),"%")
+        print("Current Income: $",sum(Movie.net_income))
+        print("Total Income(Max Income from sell of Tickets): $", Movie.total_income())
+        
+    elif n == "0":
+        break
+    else:
+        print("\n")
+        print("Please select a valid option")
+        print("\n")
---
+++ seat_book.py
@@ -0,0 +1,107 @@
+class Avaliable_seats():
+    def __init__(self):
+        self.x = int(input('Enter the number of rows:\n'))
+        self.y = int(input('Enter the number of seats in each rows:\n'))
+        self.ar_seats =[]
+    def seats(self):
+        alpha = 'A'
+        for i in range(0,self.x+1):
+            seat_list = []
+            if i == 0:
+                for i in range(0,self.y+1):
+                    if i == 0:
+                        seat_list.append(' ')
+                    else:
+                        seat_list.append(str(i))
+            else:   
+                for i in range(0,self.y+1):
+                    if i == 0:
+                        seat_list.append(alpha)
+                        alpha = chr(ord(alpha)+1)
+                    else:
+                        seat_list.append("\033[1;32m"+"S"+'\033[m')
+            self.ar_seats.append(seat_list)
+        return self.ar_seats   
+
+
+class Booking(Avaliable_seats):
+    def __init__(self):
+        self.booking_info = [["Seat No",'Name','Gender','Age','Ticket price','Phone No.']]
+        super().__init__()
+        self.seats_list = []
+        self.net_income = []
+        self.arranged_seats = self.seats()
+        self.capacity = self.x*self.y
+        
+    def info_taking(self):
+        while True:
+            while True:
+                self.seat_num = input('Enter the seat no. you want to reserve: (for eg A1, B3, D15 or E9)\n')
+                try:
+                    self.arranged_seats[ord(self.seat_num[0])-(ord("A")-1)][int(self.seat_num[1:])] == "\033[1;32mS\033[m"
+                    break
+                except:
+                    print("\n** Please insert vaild seat!! **\n")
+            if self.seat_num in self.seats_list:
+                print("Please select another seat as this seat is already booked/reserved by another person")
+            
+            else:
+                def price_fixer():
+                    a = ord(self.seat_num[0])- (ord("A")-1)
+                    ticket_Price = None
+                    if self.x*self.y<=60:
+                        ticket_Price = 10
+                    else:
+                        if self.x%2 ==1:
+                            if  a <= self.x//2:
+                                ticket_Price = 10
+                            else:
+                                ticket_Price = 8
+                        else:
+                            if a <= self.x/2:
+                                ticket_Price = 10
+                            else:
+                                ticket_Price = 8
+                    return ticket_Price
+                user_details = []
+                print("Ticket price for your seat is $", price_fixer())
+                Name = input("Enter your name:\n")
+                Gender = input("Enter your Gender: (Male/Female/Other)\n")
+                Age = input("Enter your Age:\n")
+                Ticket_price = price_fixer()
+                Phone_No = input("Enter you Phone No:\n")
+                user_details.append(self.seat_num)
+                user_details.append(Name)
+                user_details.append(Gender)
+                user_details.append(Age)
+                user_details.append('$ '+str(Ticket_price))
+                user_details.append(Phone_No)
+                self.net_income.append(Ticket_price)
+                self.booking_info.append(user_details)
+                self.seats_list.append(self.seat_num)
+                self.arranged_seats[ord(self.seat_num[0])-(ord("A")-1)][int(self.seat_num[1:])] = "\033[1;31m"+"B"+'\033[m'
+                print("\nYour ticket has been Booked successfully!!\n\nYour Details:\n")
+                print("Seat No: ",self.seat_num)
+                print("Name:",Name)
+                print("Gender:",Gender)
+                print("Age:",Age)
+                print("Phone_No.:",Phone_No)
+                return self.booking_info
+                break
+    
+    def stats(self):
+        self.occupancy = (len(self.seats_list)/self.capacity)*100
+        return self.occupancy
+    
+    def total_income(self):
+        income = None
+        if self.x*self.y <= 60 :
+            income = self.x*self.y*10
+        else:
+            top_line = self.x//2
+            back_line = self.x - (self.x//2)
+            front = top_line*self.y*10
+            back = back_line*self.y*8
+            income = front+back
+        return income
+    
